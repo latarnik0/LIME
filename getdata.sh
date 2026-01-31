40de90e9320d3b132fb62eab0de7e7b9c36088ba
latarnik0 <154380708+latarnik0@users.noreply.github.com>
2026-01-31 15:40:30 +0100

    Update output file paths in getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-grep -oE "(^cpu+[0-9]*+( +[^ ]+){7})" /proc/stat > /home/heron/monitorRes/cpud.txt
-grep -oE "(^intr+( +[^ ]+){1})|(^ctxt+( +[^ ]+){1})|(^btime+( +[^ ]+){1})|(^processes+( +[^ ]+){1})|(^procs_running+( +[^ ]+){1})|(^procs_blocked+( +[^ ]+){1})" /proc/stat > /home/heron/monitorRes/procs.txt
-ps aux > /home/heron/monitorRes/procs2.txt
-df -h | grep "/dev/*" > /home/heron/monitorRes/diskinfo.txt
+grep -oE "(^cpu+[0-9]*+( +[^ ]+){7})" /proc/stat > /home/user/monitorRes/cpud.txt
+grep -oE "(^intr+( +[^ ]+){1})|(^ctxt+( +[^ ]+){1})|(^btime+( +[^ ]+){1})|(^processes+( +[^ ]+){1})|(^procs_running+( +[^ ]+){1})|(^procs_blocked+( +[^ ]+){1})" /proc/stat > /home/user/monitorRes/procs.txt
+ps aux > /home/user/monitorRes/procs2.txt
+df -h | grep "/dev/*" > /home/user/monitorRes/diskinfo.txt
 
 
