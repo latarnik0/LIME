6bb3cd9b2275e0ba596f8353740ce1c93c6b22ed
latarnik0 <154380708+latarnik0@users.noreply.github.com>
2026-02-10 13:31:01 +0100

    Add network usage tracking to STATE structure

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@
 struct MEMORY_INFO {
 	int tot;
 	int av;
+	int usg;
 };
 
 struct CPU_STATIC_INFO {
@@ -30,21 +31,20 @@ struct CPU_STATIC_INFO {
 	std::string cachesize;
 };
 
-
-
 struct CPU_DYNAMIC_INFO {
 	long user, nice, system, idle;
-        long iowait, irq, softirq;
+    long iowait, irq, softirq;
 	float usage;
+	int usageInt;
 };
 
 struct PROCESS_INFO {
 	int interr;
-        int contextSwitches;
+    int contextSwitches;
 	int bootTime;
-        int allProcs;
-        int runningProcs;
-        int blockedProcs;
+    int allProcs;
+    int runningProcs;
+    int blockedProcs;
 	long uptimeRaw;
 	int uptimeH, uptimeM, uptimeS;
 };
@@ -55,8 +55,13 @@ struct PSAUX {
 	std::string paththrd;
 };
 
+struct NETWORK {
+	unsigned long tx;
+	unsigned long rx;
+};
+
 struct DISK_INFO {
-	std::vector<std::string> disks;
+	// TODO
 };
 
 
@@ -70,6 +75,7 @@ struct STATE {
 	PROCESS_INFO proc;
 	DISK_INFO disk;
 	PSAUX psaux;
+	NETWORK net;
 };
 
 
@@ -98,6 +104,8 @@ void read_mem(STATE &state){
 	}
 	if(config.count("MemTotal:")) { state.mem.tot = config["MemTotal:"]; }
 	if(config.count("MemAvailable:")) { state.mem.av = config["MemAvailable:"]; }
+	
+	state.mem.usg = ((state.mem.tot - state.mem.av)*100)  / state.mem.tot; 
 }
 
 // READ CPU STATIC INFO
@@ -121,16 +129,16 @@ void read_cpus(STATE &state){
 
 		while(!key.empty() && (key.back() == '\t' || key.back() == ' ')) {
             		key.pop_back();
-        	}
+        }
 
 		size_t first_char = val.find_first_not_of(" \t");
 
-        	if(first_char != std::string::npos) {
-            		val = val.substr(first_char);
-        	}
+        if(first_char != std::string::npos) {
+            	val = val.substr(first_char);
+        }
 		else{
-            		val = "";
-        	}
+            	val = "";
+        }
 
 		if(!key.empty()){
 			config[key] = val;
@@ -141,10 +149,10 @@ void read_cpus(STATE &state){
 
 
 	if(config.count("cpu cores")) { state.cpus.cores = config["cpu cores"]; }
-        if(config.count("vendor_id")) { state.cpus.vendor = config["vendor_id"]; }
+    if(config.count("vendor_id")) { state.cpus.vendor = config["vendor_id"]; }
 	if(config.count("cpu family")) { state.cpus.cpufamily = config["cpu family"]; }
 	if(config.count("model name")) { state.cpus.modelname = config["model name"]; }
-        if(config.count("cpu MHz")) { state.cpus.mhz = config["cpu MHz"]; }
+    if(config.count("cpu MHz")) { state.cpus.mhz = config["cpu MHz"]; }
 	if(config.count("cache size")) { state.cpus.cachesize = config["cache size"]; }
 }
 
@@ -202,6 +210,7 @@ void read_cpud(STATE &state){
     totalDiff = (currIdle - prevIdle)+(currActive - prevActive);
     
     state.curr.usage = ((static_cast<float>(activeDiff)) / (static_cast<float> (totalDiff*100.0)));
+	state.curr.usageInt = static_cast<int>(state.curr.usage);
 }
 
 
@@ -220,40 +229,40 @@ void read_uptime(STATE &state){
 	state.proc.uptimeRaw = std::stoi(line);
 	
 	state.proc.uptimeH = state.proc.uptimeRaw / 3600;
-        state.proc.uptimeRaw %= 3600;
+    state.proc.uptimeRaw %= 3600;
 
-        state.proc.uptimeM = state.proc.uptimeRaw / 60;
-        state.proc.uptimeS = state.proc.uptimeRaw % 60;
+    state.proc.uptimeM = state.proc.uptimeRaw / 60;
+    state.proc.uptimeS = state.proc.uptimeRaw % 60;
 }
 
 
 // PROCS INFO 1
 void read_procs(STATE &state){
 	std::string line;
-       	int hours, minutes, seconds;
+    int hours, minutes, seconds;
 	std::map<std::string, int> config;
-        std::ifstream infoProcs("/proc/stat");
+    std::ifstream infoProcs("/proc/stat");
 
-        if(!infoProcs.is_open()){
-                std::cerr<<"Cannot open file stat (processes)"<<std::endl;
-        }
+    if(!infoProcs.is_open()){
+            std::cerr<<"Cannot open file stat (processes)"<<std::endl;
+    }
 
-        while(std::getline(infoProcs, line)){
-                std::stringstream ss(line);
-                std::string key;
-                int val;
-                ss >> key >> val;
+    while(std::getline(infoProcs, line)){
+        	std::stringstream ss(line);
+            std::string key;
+            int val;
+            ss >> key >> val;
 
-                if(!key.empty()){
-                        config[key] = val;
-                }
-        }
-        if(config.count("intr")) { state.proc.interr = config["intr"]; }
-        if(config.count("ctxt")) { state.proc.contextSwitches = config["ctxt"]; }
+            if(!key.empty()){
+                    config[key] = val;
+            }
+    }
+    if(config.count("intr")) { state.proc.interr = config["intr"]; }
+    if(config.count("ctxt")) { state.proc.contextSwitches = config["ctxt"]; }
 	if(config.count("btime")) { state.proc.bootTime = config["btime"]; }
-        if(config.count("processes")) { state.proc.allProcs = config["processes"]; }
+    if(config.count("processes")) { state.proc.allProcs = config["processes"]; }
 	if(config.count("procs_running")) { state.proc.runningProcs = config["procs_running"]; }
-        if(config.count("procs_blocked")) { state.proc.blockedProcs = config["procs_blocked"]; }
+    if(config.count("procs_blocked")) { state.proc.blockedProcs = config["procs_blocked"]; }
 	
 }
 
@@ -305,6 +314,41 @@ void count_active_ps(STATE &state){
         }
 }
 
+
+void read_network(STATE &state){
+    std::ifstream data("/proc/net/dev");
+    std::string line;
+    int ignoreThis;
+    unsigned long rxb, txb;
+	state.net.rx = 0;
+	state.net.tx = 0;
+
+    std::getline(data, line);
+    std::getline(data, line);
+
+    while(std::getline(data, line)){
+            std::stringstream ss(line);
+            std::string interfaceName;
+
+            ss >> interfaceName;
+
+            if(interfaceName == "lo") { continue; }
+            else{
+				ss >> rxb;
+                state.net.rx += rxb;
+
+                for(int i=0; i<7; i++){
+					ss >> ignoreThis;
+                }
+
+                ss >> txb;
+                state.net.tx += txb;
+            }
+    }
+}
+
+
+
 // PRINT STATE
 void print_res(STATE &state){
 
@@ -312,15 +356,19 @@ void print_res(STATE &state){
 	getmaxyx(stdscr, max_y, max_x);
 
 	start_color();
-        use_default_colors();
+    use_default_colors();
 
-        init_pair(1, COLOR_RED, COLOR_WHITE);
-        init_pair(2, COLOR_BLUE, COLOR_WHITE);
-        init_pair(3, COLOR_WHITE, -1);
+    init_pair(1, COLOR_RED, COLOR_WHITE);
+    init_pair(2, COLOR_BLUE, COLOR_WHITE);
+    init_pair(3, COLOR_WHITE, -1);
+	init_pair(4, COLOR_GREEN, COLOR_BLACK);
+	init_pair(5, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(6, COLOR_RED, COLOR_BLACK);
+	init_pair(7, COLOR_BLACK, COLOR_BLACK);
 
 
 //----------------------------------STATIC INFO----------------------------------------------------------
-    	attron(COLOR_PAIR(2));
+    attron(COLOR_PAIR(2));
 	attron(A_BOLD);
 	attron(A_STANDOUT);
 	mvprintw(0, 20, "STATIC INFO");
@@ -339,25 +387,25 @@ void print_res(STATE &state){
 
 	mvprintw(4, 0, "Vendor"); 
 	printw(" : %s ", (state.cpus.vendor).c_str());
-        mvprintw(5, 0, "Family");
-        printw(" : %s ", (state.cpus.cpufamily).c_str());
-        mvprintw(6, 0, "Model Name");
-        printw(" : %s ", (state.cpus.modelname).c_str());
-        mvprintw(7, 0, "Cores");
-        printw(" : %s ", (state.cpus.cores).c_str());
-        mvprintw(8, 0, "Clock speed:"); 
-        printw(" : %s ", (state.cpus.mhz).c_str());
+    mvprintw(5, 0, "Family");
+    printw(" : %s ", (state.cpus.cpufamily).c_str());
+    mvprintw(6, 0, "Model Name");
+    printw(" : %s ", (state.cpus.modelname).c_str());
+    mvprintw(7, 0, "Cores");
+    printw(" : %s ", (state.cpus.cores).c_str());
+    mvprintw(8, 0, "Clock speed:"); 
+    printw(" : %s ", (state.cpus.mhz).c_str());
 	printw(" MHz");
 
 
 
 	attron(COLOR_PAIR(3));
-        attron(A_BOLD);
-        attron(A_STANDOUT);
-        mvprintw(10, 20, "Disks Usage");
-        attroff(COLOR_PAIR(3));
-        attroff(A_BOLD);
-        attroff(A_STANDOUT);
+    attron(A_BOLD);
+    attron(A_STANDOUT);
+    mvprintw(10, 20, "Disks Usage");
+    attroff(COLOR_PAIR(3));
+    attroff(A_BOLD);
+    attroff(A_STANDOUT);
 
 	
 
@@ -368,25 +416,51 @@ void print_res(STATE &state){
 	attron(A_STANDOUT);
 	mvprintw(0, 80, "RESOURCES");
 	attroff(COLOR_PAIR(1));
-        attroff(A_BOLD);
-        attroff(A_STANDOUT);
+    attroff(A_BOLD);
+    attroff(A_STANDOUT);
 
 	attron(COLOR_PAIR(3));
-        attron(A_BOLD);
-        attron(A_STANDOUT);
-        mvprintw(3, 83, "RAM");
-        attroff(COLOR_PAIR(3));
-        attroff(A_BOLD);
-        attroff(A_STANDOUT);
+    attron(A_BOLD);
+    attron(A_STANDOUT);
+    mvprintw(3, 83, "RAM");
+    attroff(COLOR_PAIR(3));
+    attroff(A_BOLD);
+    attroff(A_STANDOUT);
 
 
-	
-    	mvprintw(4, 70, "Total RAM : %.2f", static_cast<float> (state.mem.tot / 1000.0));
+    mvprintw(4, 70, "Total RAM : %.2f", static_cast<float> (state.mem.tot / 1000.0));
 	printw(" MB");
-    	mvprintw(5, 70, "Available RAM : %.2f", static_cast<float> (state.mem.av / 1000.0));
+    mvprintw(5, 70, "Available RAM : %.2f", static_cast<float> (state.mem.av / 1000.0));
 	printw(" MB");
 	mvprintw(6, 70, "Usage : %.2f", static_cast<float> (((state.mem.tot/1000.0) - (state.mem.av/1000.0) ) / (state.mem.tot/1000.0) * 100.0));
 	printw("%%");
+	mvprintw(7, 70, "[");
+	for(int i=0; i<50; i++){
+        if(i<state.mem.usg/2){
+            if(i<=30){
+                attron(COLOR_PAIR(4));
+                printw("|");
+                attroff(COLOR_PAIR(4));
+            }
+        	else if(i>30 && i<=40){
+                attron(COLOR_PAIR(5));
+                printw("|");
+                attroff(COLOR_PAIR(5));
+        	}
+        else{
+            attron(COLOR_PAIR(6));
+            printw("|");
+            attroff(COLOR_PAIR(6));
+        	}
+        }
+    	else{
+                        attron(COLOR_PAIR(7));
+                        printw(" ");
+                        attroff(COLOR_PAIR(7));
+                }
+        }
+
+	mvprintw(7, 120, "]");
 
 
 	attron(COLOR_PAIR(3));
@@ -399,12 +473,42 @@ void print_res(STATE &state){
 
         mvprintw(11, 70, "All cores: %.2f", state.curr.usage);
         printw(" %%");
+ 	
+	mvprintw(12, 70, "[");
+
+	for(int i=0; i<50; i++){
+                if(i<state.curr.usageInt/2){
+			if(i<=30){
+                        	attron(COLOR_PAIR(4));
+                        	printw("|");
+                        	attroff(COLOR_PAIR(4));
+                	}
+                	else if(i>30 && i<=40){
+                        	attron(COLOR_PAIR(5));
+                        	printw("|");
+                        	attroff(COLOR_PAIR(5));
+                	}
+                	else{
+                        	attron(COLOR_PAIR(6));
+                        	printw("|");
+                        	attroff(COLOR_PAIR(6));
+                	}
+		}
+		else{
+			attron(COLOR_PAIR(7)); 
+        		printw(" ");           
+			attroff(COLOR_PAIR(7));
+		}
+        }
+        mvprintw(12, 120, "]");
+
 	
 
 	mvprintw(0, 140, "Uptime: %02d:%02d:%02d", state.proc.uptimeH, state.proc.uptimeM, state.proc.uptimeS);
 	mvprintw(2, 140, "Kernel threads: %d", state.psaux.kthrd);
 	mvprintw(3, 140, "User threads: %d", state.psaux.uthrd);
-	mvprintw(4, 140, "All tasks: %d", state.psaux.uthrd + state.psaux.kthrd);
+	mvprintw(4, 140, "Receive (bytes): %ld", state.net.rx);
+	mvprintw(5, 140, "Transmit (bytes): %ld", state.net.tx);
 }
 
 
@@ -429,6 +533,7 @@ int main(){
 		read_cpus(state);
 		read_cpud(state);
 		count_active_ps(state);
+		read_network(state);
 			
 		print_res(state);
 		refresh();
